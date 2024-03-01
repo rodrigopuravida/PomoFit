8f6d303c279701b665278808b35125f09f72973b
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-02-29 21:50:46 -0800

    Initial Commit

---
+++ PomoFit/PomoFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoFitApp.swift
+//  PomoFit
+//
+//  Created by Rodrigo Carballo on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PomoFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
