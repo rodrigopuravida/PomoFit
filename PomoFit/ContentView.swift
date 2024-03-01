c3629fa5dd912a04eefa010da38de30cbaef8488
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-02-29 22:55:06 -0800

    First set of ideas - alider, button

--- PomoFit/ContentView.swift
+++ PomoFit/ContentView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+
+  @State private var focusTime: Double = 0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+          Text("Time in Minutes")
+          Slider(value: $focusTime, in: 0...60)
+          Text("\(focusTime, specifier: "%.0f")")
+
+            Image("Pomo")
+            .resizable()
+            .frame(width:200, height: 200)
+
+          Button {
+            print("Clicked the Start")
+          }
+            label: {
+              Label("Start", systemImage: "pencil")
+                .padding()
+                .foregroundStyle(.white)
+                .background(.red)
+
+              }
+            }
+          }
     }
-}
+
 
 #Preview {
     ContentView()
