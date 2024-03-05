9cd6e4253e527e1cf1bc18bd670ba682389d7be8
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-03-05 06:31:32 -0800

    Commiting Timer and Lock screen

--- PomoFit/ContentView.swift
+++ PomoFit/ContentView.swift
@@ -6,34 +6,84 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
 
-  @State private var focusTime: Double = 0
+  //@State private var focusTime: Double = .zero
+  @State private var timeRemaining : Double = 10
+  let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
+
+  @Environment(\.scenePhase) var scenePhase
+  @State private var isActive = true
+
 
     var body: some View {
+      ZStack {
         VStack {
-          Text("Time in Minutes")
-          Slider(value: $focusTime, in: 0...60)
-          Text("\(focusTime, specifier: "%.0f")")
+            Text("Time in Minutes")
+              .font(.title)
+          Slider(value: $timeRemaining, in: 0...60)
+            Text("\(abs(timeRemaining), specifier: "%.0f")")
+              .font(.title)
 
-            Image("Pomo")
-            .resizable()
-            .frame(width:200, height: 200)
+              Image("Pomo")
+              .resizable()
+              .frame(width:200, height: 200)
 
-          Button {
-            print("Clicked the Start")
-          }
-            label: {
-              Label("Start", systemImage: "pencil")
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
-
-              }
+          /*
+            Button {
+              print("Clicked the Start")
             }
+              label: {
+                Label("Start", systemImage: "pencil")
+                  .padding()
+                  .foregroundStyle(.white)
+                  .background(.red)
+
+                }
+              .padding()
+
+
+            Text("Time left : \(focusTime, specifier: "%.0f")")
+              .font(.largeTitle)
+ */
+
+          Text("Time: \(abs(timeRemaining), specifier: "%.0f")")
+              .font(.largeTitle)
+              .foregroundStyle(.white)
+              .padding(.horizontal, 20)
+              .padding(.vertical, 5)
+              .background(.black.opacity(0.75))
+              .clipShape(.capsule)
+
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+          if timeRemaining == 0 {
+            //AudioServicesPlaySystemSound(1026)
           }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
+            }
+        }
+        .onAppear{UIApplication.shared.isIdleTimerDisabled = true
+        }
+        .onDisappear {
+          UIApplication.shared.isIdleTimerDisabled = false
+        }
+      }
+        }
     }
+
 
 
 #Preview {
