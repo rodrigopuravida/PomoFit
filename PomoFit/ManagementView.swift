46fb0ec9b5ed43ed555fd36393c3f36243fd2523
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-03-06 20:29:16 -0800

    Adding Management screen and navigation to running screen

---
+++ PomoFit/ManagementView.swift
@@ -0,0 +1,18 @@
+//
+//  ManagementView.swift
+//  PomoFit
+//
+//  Created by Rodrigo Carballo on 3/5/24.
+//
+
+import SwiftUI
+
+struct ManagementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManagementView()
+}
